5fb9c1a35d41c5482451079369cdfcb6ed026a0c
Abdimusa <musa301196@gmail.com>
2021-09-27 19:03:19 +0300

    generics

--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -8,57 +8,50 @@ from rest_framework.generics import get_object_or_404
 
 
 
-class TaskAPIView(APIView):
+class TaskCreateView(generics.ListCreateAPIView): # Очень коротко
     permission_classes = [IsAuthenticated, ]
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
 
-    def get(self, request):
-        tasks = Task.objects.all()
-        serializer = TaskSerializer(tasks, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = TaskSerializer(data=request.data)
-
-        if serializer.is_valid():
-            owner = request.user
-            task = Task.objects.create(
-                owner=owner,
-                text=serializer.validated_data.get('text'),
-                estimated_finish_time=serializer.validated_data.get('estimated_finish_time')
-            )
-            serializer = TaskSerializer(instance=task)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response({'detail': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+    def perform_create(self, serializer):
+        '''
+        Функция для сохранения нынешнего пользователя владельцем созданного поста
+        '''
+        return serializer.save(owner=self.request.user)
 
 
-class TaskDetailAPIView(APIView):
+class TaskAllGenericView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticated, ]
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    lookup_field = 'id'
 
-    def get(self, request, id):
-        task = get_object_or_404(Task, id=id)
-        serializer = TaskSerializer(task)
-        return Response(serializer.data)
 
-    def put(self, request, id):
-        task = get_object_or_404(Task, id=id)
-        serializer = TaskSerializer(data=request.data)
-        if serializer.is_valid():
-            update_fields = []
-            for field, value in serializer.validated_data.items():
-                setattr(task, field, value)
-                update_fields.append(field)
-            task.save(update_fields=update_fields)
 
-            serializer = TaskSerializer(instance=task)
-            return Response(serializer.data)
-        return Response({'detail': serializer.errors},
-                        status=status.HTTP_400_BAD_REQUEST)
+# ------------------ второй более простой подход
 
-    def delete(self, request, id):
-        task = get_object_or_404(Task, id=id)
-        task.delete()
-        return Response({'detail': f'deleted object #{id}'}, 
-                        status=status.HTTP_204_NO_CONTENT)
+# class TaskListView(generics.ListAPIView):
+#     permission_classes = [IsAuthenticated,]
+#     queryset = Task.objects.all()
+#     serializer_class = TaskSerializer
+
+
+
+
+# class TaskUpdateGenericView(generics.UpdateAPIView):
+#     permission_classes = [IsAuthenticated, ]
+#     queryset = Task.objects.all()
+#     serializer_class = TaskSerializer
+#     lookup_field = 'id'
+
+
+# class TaskDestroyGenericView(generics.DestroyAPIView):
+#     permission_classes = [IsAuthenticated, ]
+#     queryset = Task.objects.all()
+#     serializer_class = TaskSerializer
+#     lookup_field = 'id'
+
+# можно либо так, как внизу с двумя функциями 
 
 class SetFinishedTaskAPIView(APIView):
 
